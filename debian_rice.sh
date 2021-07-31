37a8fcd366c0b5d31304c9bbf694d5f3135c5f60
inugami-dev64 <jxwest@protonmail.com>
2021-07-31 13:47:48 +0300

    Add some scripts for mpv

--- debian_rice.sh
+++ debian_rice.sh
@@ -1,8 +1,15 @@
-#/bin/sh
-MOD_PATH=./submodules
+#/bin/sh MOD_PATH=./submodules
+MOD_PATH="submodules"
+YT_DLP_VERSION=2021.07.24
 
 if [ -p "$MOD_PATH" ]; then
     echo "Please initialise git submodules"
+    exit
+fi
+
+DOAS_PATH=$(which doas)
+if [ -z "$DOAS_PATH" ]; then
+    echo "Could not find doas executable, please install doas before continuing with the script!"
     exit
 fi
 
@@ -44,6 +51,27 @@ doas apt install -y \
     python3 \
     python2 \
     xinit
+
+# Remove youtube-dl and replace its executable with yt-dlp one
+doas apt remove -y youtube-dl
+wget https://github.com/yt-dlp/yt-dlp/releases/download/$YT_DLP_VERSION/yt-dlp
+wget https://github.com/yt-dlp/yt-dlp/releases/download/$YT_DLP_VERSION/SHA2-512SUMS
+
+# Verify checksums
+grep "yt-dlp$" SHA2-512SUMS > checksum
+rm -rf SHA2-512SUMS
+
+if [[ "$(sha512sum -c checksum)" != "yt-dlp: OK" ]]; then
+    echo "Failed to verify yt-dlp checksum"
+    exit
+fi
+
+echo "yt-dlp integrity verified"
+chmod 0755 yt-dlp 
+rm -rf checksum
+doas mv yt-dlp /usr/local/bin/youtube-dl
+
+
 
 # Copy all .config files over
 if [[ -d "~/.config" ]]; then
