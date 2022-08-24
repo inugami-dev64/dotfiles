a2d1a92128c1a27fa7528f324741d26699ab02ae
inugami-dev64 <karl@karlott.net>
2022-08-24 16:43:17 +0300

    Write firefox configuration

---
+++ firefox/user-overrides.js
@@ -0,0 +1,8 @@
+// Enable adding custom search engines
+user_pref("browser.urlbar.update2.engineAliasRefresh", true);
+
+// Disable pocket
+user_pref("extensions.pocket.enabled", false);
+
+// Enable urlbar searches
+user_pref("keyword.enabled", true);
