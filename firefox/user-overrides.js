e0cd533698fe2ea91690873d053f6318a1bb7633
inugami-dev64 <karl@karlott.net>
2024-07-19 16:30:37 +0300

    Remove user.js from firefox directory

--- firefox/user-overrides.js
+++ firefox/user-overrides.js
@@ -11,28 +11,28 @@ user_pref("extensions.pocket.enabled", false);
 user_pref("keyword.enabled", true);
 
 // enable WebRTC
-user_pref("media.peerconnection.enabled", true)
-user_pref("media.peerconnection.ico.no_host", false)
-user_pref("media.peerconnection.ice.proxy_only_if_behind_proxy", false)
+user_pref("media.peerconnection.enabled", true);
+user_pref("media.peerconnection.ico.no_host", false);
+user_pref("media.peerconnection.ice.proxy_only_if_behind_proxy", false);
 user_pref("media.peerconnection.ice.default_address_only", true);
 
 // needed for screensharing
 user_pref("media.getusermedia.screensharing.enabled", true);
 
 // screen flicker in Zoon, this should be false
-user_pref("layers.acceleration.force-enabled", false)
+user_pref("layers.acceleration.force-enabled", false);
 
 // disable resistFingerprinting
-user_pref("privacy.resistFingerprinting", false)
+user_pref("privacy.resistFingerprinting", false);
 
 // disable letterboxing
-user_pref("privacy.resistFingerprinting.letterboxing", false)
+user_pref("privacy.resistFingerprinting.letterboxing", false);
 
 // enable mozAddonManager
 user_pref("privacy.resistFingerprinting.block_mozAddonManager", false);
 
 // do not disable asm.js
-user_pref("javascript.options.asmjs", true)
+user_pref("javascript.options.asmjs", true);
 
 // do not disable wasm
 user_pref("javascript.options.wasm", true);
