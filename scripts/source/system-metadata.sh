abcea7026da42eba1c517050a40242be48a51d38
inugami-dev64 <karmen@nyaa.ee>
2026-07-12 16:18:39 +0300

    Create scripts and a systemd service for setting maximum battery charge limit

---
+++ scripts/source/system-metadata.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# Exports following system metadata variables
+#  - DISTRO - Base Linux distribution where the system is running at
+#  - MANUFACTURER - Computer manufacturer name
+#  - HAS_BATTERY - Set to 1 if current system has a battery, 0 otherwise
+
+# Set DISTRO variable
+ID="$(grep -E --color=none '^ID=[a-z0-9_]+$' /etc/os-release | sed -r 's/ID=([a-z0-9_]+)/\1/g')"
+ID_LIKE="$(grep -E --color=none '^ID_LIKE=[a-z0-9_]+$' /etc/os-release | sed -r 's/ID_LIKE=([a-z0-9_]+)/\1/g')"
+
+if [ -n "$ID_LIKE" ]; then
+    export DISTRO="$ID_LIKE"
+else
+    export DISTRO="$ID"
+fi
+
+# Computer manufacturer
+export MANUFACTURER="$(dmidecode | grep -A1 --color=none '^System Information' | grep --color=none 'Manufacturer' | sed -r 's/^\t+Manufacturer: ([a-z0-9A-Z ]+)$/\1/g')"
+
+# Set HAS_BATTERY variable
+if [ -n "$(ls /sys/class/power_supply/BAT* 2>/dev/null)" ]; then
+    export HAS_BATTERY=0
+else
+    export HAS_BATTERY=1
+fi
